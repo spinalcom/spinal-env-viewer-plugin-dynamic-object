1172873e042e9351362534b644e7de3c3d4c9bad
TheCoolNickName <45173844+TheCoolNickName@users.noreply.github.com>
2018-12-20 12:37:18 +0100

    Update customDynamicObject.vue

--- vue/customDynamicObject.vue
+++ vue/customDynamicObject.vue
@@ -19,81 +19,88 @@ with this file. If not, see
 -->
 
 <template>
-  <div class="delete-panel-container">
-      <div class="warningMessage">
-          <p class="warningDelete">Custom your object</p>
-    </div>
-    <div class="md-layout md-input-div center-div">
-            <p>name : <input v-model="name">
-            <md-button class="md-button-accept"
-                 v-on:click="changeName">Change</md-button>
-            </p>
-            <br>
-            <p>x : <input v-model="x" style="margin-left:30px">
-            <md-button class="md-button-accept"
-                 v-on:click="changeX">Change</md-button>
-            </p>
-            <br>
-            <p>y : <input v-model="y" style="margin-left:30px">
-            <md-button class="md-button-accept"
-                 v-on:click="changeY">Change</md-button>
-            </p>
-            <br>
-            <p>z : <input v-model="z" style="margin-left:30px">
-            <md-button class="md-button-accept"
-                 v-on:click="changeZ">Change</md-button>
-            </p>
-            <br>
-            <p>color :  
-              <select v-model="color" style="margin-right:74px; margin-left:7px;width: 107px;">
+  <div>
+      <md-button v-on:click="changeValue" class="HeaderButton delete-panel-container">Custom your object</md-button>
+            <p class="title">name : <input v-model="name"></p>
+
+              <p class="title">Change X :</p>
+            <div style="margin-left: 30px; margin-right: 90px;">
+               <vue-slider ref="slider" v-bind="xValue" v-model="x"></vue-slider>
+            </div> 
+              <input type="number" v-model="x" class="slideWithInput" >
+
+            <p class="title">Change Y :</p>
+            <div style="margin-left: 30px; margin-right: 90px;">
+               <vue-slider ref="slider" v-bind="yValue" v-model="y"></vue-slider>
+            </div> 
+              <input type="number" v-model="y" class="slideWithInput" >
+
+            <p class="title">Change Z :</p>
+            <div style="margin-left: 30px; margin-right: 90px;">
+               <vue-slider ref="slider" v-bind="zValue" v-model="z"></vue-slider>
+            </div> 
+              <input type="number" v-model="z" class="slideWithInput" >
+
+            <p class="title">Change Radius :</p>
+            <div style="margin-left: 30px; margin-right: 90px;">
+               <vue-slider ref="slider" v-bind="RadiusValue" v-model="radius"></vue-slider>
+            </div> 
+              <input type="number" v-model="radius" class="slideWithInput" >
+
+            <p class="titleInput">Change Form :</p>
+            <select class="selectedInput" style="margin-left: 150px;" v-model="formSelected">
+                <option v-for="option in form" v-bind:value="option">
+                 {{ option }}
+                </option>
+            </select>
+
+            <p class="titleInput">Move element with click </p>
+              <md-button class="md-button-accept selectedInput"
+                style="margin-left: 210px;" v-on:click="getNewPosition">Change</md-button>
+
+            <p class="titleInput">Change Color :</p>
+              <select class="selectedInput" v-model="color" style="margin-left: 150px;">
                  <option v-for="option in optionsColor" v-bind:value="option">
                  {{ option }}
                 </option>
               </select>
 
-                      <!-- <input v-model="color" style="margin-left:6px"> -->
-            <md-button class="md-button-accept"
-                 v-on:click="changeColor">Change</md-button>
-            </p>
-            <br>
-            <p>radius : <input v-model="radius" style="margin-left:1px">
-            <md-button class="md-button-accept"
-                 v-on:click="changeRadius">Change</md-button>
-            </p>
-
-            <br>
-            <p>form :  
-              <select v-model="formSelected" style="margin-right:74px; margin-left:7px;width: 107px;">
-                 <option v-for="option in form" v-bind:value="option">
-                 {{ option }}
-                </option>f
-              </select>
-
-                      <!-- <input v-model="color" style="margin-left:6px"> -->
-            <md-button class="md-button-accept"
-                 v-on:click="changeForm">Change</md-button>
-            </p>
-<!--             <md-button class="md-button-cancel"
-                 v-on:click="cancelMethod">Cancel</md-button> -->
-
-            <br>
-            <p>move element with click 
-            <md-button class="md-button-accept"
-                 v-on:click="getNewPosition">Change</md-button>
-            </p>
-    </div>
-    <hr/>
 
   </div>
 </template>
 
 <script>
+import vueSlider from 'vue-slider-component/src/vue2-slider.vue'
 import { SpinalGraphService } from 'spinal-env-viewer-graph-service';
 import THREEObject from '../THREE';
 export default {
   name: "my_compo",
+  components: {
+    vueSlider
+  },
   data() {
     return {
+      xValue: {
+        min: -300,
+        max: 300,
+        tooltip: false
+      },
+      yValue: {
+        min: -300,
+        max: 300,
+        tooltip: false
+      },
+      zValue: {
+        min: -300,
+        max: 300,
+        tooltip: false
+      },
+      RadiusValue: {
+        min: 0,
+        max: 25,
+        tooltip: false
+      },
+      value: 1,
       currentNode: "",
       x: 0,
       y: 0,
@@ -115,7 +122,7 @@ export default {
     };
   },
   methods: {
-    opened(option, viewer) {
+    opened(option) {
       this.currentNode = option.selectedNode;
       this.x = option.selectedNode.x.get();
       this.y = option.selectedNode.y.get();
@@ -126,97 +133,58 @@ export default {
       this.name = option.selectedNode.name.get();
       this.THREE = new THREEObject();
     },
-    changeX(option, viewer) {
+    changeX(){
       this.currentNode.x.set(this.x);
       this.THREE.CustomObject(this.currentNode);
-//      console.log(this.currentNode.x.get())
 
       let realNode = SpinalGraphService.getRealNode(this.currentNode.id.get());
-      realNode.x.color.set(this.currentNode.x.get());
+      realNode.info.x.set(this.currentNode.x.get());
     },
-    changeY(option, viewer) {
+    changeY(){
       this.currentNode.y.set(this.y);
-      //this.THREE.DeleteObject();
       this.THREE.CustomObject(this.currentNode);
 
       let realNode = SpinalGraphService.getRealNode(this.currentNode.id.get());
       realNode.info.y.set(this.currentNode.y.get());
     },
-    changeZ(option, viewer) {
-      // this.THREE.DeleteObject(this.currentNode);
+    changeZ(){
       this.currentNode.z.set(this.z);
-      //this.THREE.DeleteObject();
-      // this.THREE.NewObject(this.currentNode);
-
-
-     // this.THREE.DeleteObject(this.currentNode.selectedNode.id.get())
-
-      //SpinalGraphService.deleteElement(this.currentNode.id.get());
-
-      //SpinalGraphService.setElement(id, {name: `${name}`, x: `${x}`, y: `${y}`, z: `${z}`, color: `${color}`, radius: `${radius}`, heightSegments: `${heightSegments}`, widthSegments: `${widthSegments}`})
-
-
-      //this.THREE.NewObject(this.currentNode);
-
       this.THREE.CustomObject(this.currentNode);
 
       let realNode = SpinalGraphService.getRealNode(this.currentNode.id.get());
       realNode.info.z.set(this.currentNode.z.get());
-      //console.log(realNode);
     },
-    changeColor(option, viewer) {
+    changeColor() {
       this.currentNode.color.set(this.color);
-      //this.THREE.DeleteObject();
-
       this.THREE.CustomObjectColor(this.currentNode);
 
       let realNode = SpinalGraphService.getRealNode(this.currentNode.id.get());
       realNode.info.color.set(this.currentNode.color.get());
-      console.log(this.currentNode.color.get());
     },
-    changeName(option, viewer) {
-      //let name = this.currentNode.name.get();
+    changeName() {
       this.currentNode.name.set(this.name);
-      //this.THREE.DeleteObject();
-      //this.THREE.CustomObject(this.currentNode, name);
 
       let realNode = SpinalGraphService.getRealNode(this.currentNode.id.get());
       realNode.info.name.set(this.currentNode.name.get());
     },
-    changeRadius(option, viewer) {
-      //this.THREE.DeleteObject(this.currentNode);
+    changeRadius() {
       this.currentNode.radius.set(this.radius);
-      //this.THREE.DeleteObject();
       this.THREE.CustomObjectRadius(this.currentNode);
 
       let realNode = SpinalGraphService.getRealNode(this.currentNode.id.get());
       realNode.info.radius.set(this.currentNode.radius.get());
-
-      //console.log(this.currentNode);
-    //  SpinalGraphService.removeFromGraph(this.currentNode.selectedNode.id.get())
-      //this.currentNode.selectedNode.removeFromGraph();
-    //  let el = document.getElementsByClassName("dockingPanelClose");
-    //  el[4].click();
-      //  removeChild( nodeId, childId, relationName, relationType, stop = false )
     },
-    changeForm(option, viewer) {
-      //this.THREE.DeleteObject(this.currentNode);
-      // this.currentNode.radius.set(this.radius);
-      // //this.THREE.DeleteObject();
-      // this.THREE.CustomObjectRadius(this.currentNode);
+    changeForm() {
       this.currentNode.form.set(this.formSelected);
       this.THREE.CustomObjectForm(this.currentNode);
 
       let realNode = SpinalGraphService.getRealNode(this.currentNode.id.get());
       realNode.info.form.set(this.currentNode.form.get());
-      // let realNode = SpinalGraphService.getRealNode(this.currentNode.id.get());
-      // realNode.info.radius.set(this.currentNode.radius.get());
-      //console.log("do stuff ", this.formSelected);
     },
     changePosition: function(e) {
       let clicked = window.spinal.ForgeViewer.viewer.clientToWorld(e.layerX, e.layerY, false);
       let realNode = SpinalGraphService.getRealNode(this.currentNode.id.get());
-      
+
       if (clicked) {
         this.x = Math.round(clicked.intersectPoint.x);
         this.y = Math.round(clicked.intersectPoint.y);
@@ -224,9 +192,9 @@ export default {
         this.currentNode.x.set(this.x);
         this.currentNode.y.set(this.y);
         this.currentNode.z.set(this.z);
-        realNode.info.x.set(this.x);
-        realNode.info.z.set(this.z);
-        realNode.info.y.set(this.y);
+        realNode.info.x.set(this.currentNode.x.get());
+        realNode.info.y.set(this.currentNode.y.get());
+        realNode.info.z.set(this.currentNode.z.get());
       }
       document.removeEventListener("mousedown", this.changePosition, true);
       this.THREE.CustomObject(this.currentNode);
@@ -234,21 +202,43 @@ export default {
     getNewPosition() {
       document.addEventListener("mousedown", this.changePosition, true);
     },
-    cancelMethod(option, viewer) {
-    //  let el = document.getElementsByClassName("dockingPanelClose");
-    //  el[4].click();
+    changeValue() {
+      if (this.x != this.currentNode.x.get()) {
+        console.log("x changed");
+        this.changeX();
+      } if (this.y !== this.currentNode.y.get()) {
+        console.log("y changed");
+        this.changeY();
+      } if (this.z !== this.currentNode.z.get()) {
+        console.log("z changed");
+        this.changeZ();
+      } if (this.radius !== this.currentNode.radius.get()) {
+        console.log("radius changed");
+        this.changeRadius();
+      } if (this.formSelected !== this.currentNode.form.get()) {
+        console.log("form changed");
+        this.changeForm();
+      } if (this.color !== this.currentNode.color.get()) {
+        console.log("color changed");
+        this.changeColor();
+      } if (this.name !== this.currentNode.name.get()) {
+        console.log("name changed");
+        this.changeName();
+      }
     }
   }
 };
 </script>
 
-<style>
+<style scoped>
 .delete-panel-container * {
   box-sizing: border-box;
 }
-.warningDelete {
+.HeaderButton {
   text-align: center;
   font-size: 120%;
+  background-color: #3C3B46;
+  width: 380px;
 }
 .md-button-accept {
   background-color: #356BAB;
@@ -262,5 +252,26 @@ export default {
 }
 .center-div {
   margin-left: 40px;
+}
+.title {
+  margin: auto;
+  margin-left: 40px;
+}
+.titleInput {
+  margin-top: 3px;
+  margin: auto;
+  margin-left: 40px;
+  height: 30px;
+}
+.selectedInput {
+  margin-top:-30px;
+  width:100px;
+  position: absolute;
+}
+.slideWithInput {
+  width: 50px;
+  position: absolute;
+  margin-left: 310px;
+  margin-top:-20px;
 }
 </style>
